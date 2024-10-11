e123f745119a9243d6a34dd1a0a5db7967e908f4
0xc0debabe <kongminoo@naver.com>
2024-10-11 15:22:18 +0900

    [24/10/11] JdbcTemplate

---
+++ src/main/java/hello/itemservice/config/JdbcTemplateV3Config.java
@@ -0,0 +1,31 @@
+package hello.itemservice.config;
+
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.jdbctemplate.JdbcTemplateItemRepositoryV2;
+import hello.itemservice.repository.jdbctemplate.JdbcTemplateItemRepositoryV3;
+import hello.itemservice.service.ItemService;
+import hello.itemservice.service.ItemServiceV1;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+@RequiredArgsConstructor
+public class JdbcTemplateV3Config {
+
+    // datasource와 connection pool은 application.properties에서 작성된대로 자동 등록됨
+    private final DataSource dataSource;
+
+    @Bean
+    public ItemService itemService() {
+        return new ItemServiceV1(itemRepository());
+    }
+
+    @Bean
+    public ItemRepository itemRepository() {
+        return new JdbcTemplateItemRepositoryV3(dataSource);
+    }
+
+}
